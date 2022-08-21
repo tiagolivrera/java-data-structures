1a7e75521f82798c1ba15b3dafaa18a89ffdf24f
Thyago de Oliveira Freitas <thyago.olifreitas@gmail.com>
2022-08-21 10:07:08 -0300

    Implementando FilaFIFO

--- src/App.java
+++ src/App.java
@@ -1,24 +1,11 @@
+import models.FilaFIFO;
 import models.Pilha;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
         Pilha p1 = new Pilha();
+        FilaFIFO f1 = new FilaFIFO();
 
-        p1.push(1);
-        p1.push(2);
-        p1.push(3);
-        p1.push(4);
-        p1.push(5);
-
-        System.out.println(p1.toString() + " Size: " + p1.size());
-
-        p1.pop();
-        p1.pop();
-        p1.pop();
-        p1.pop();
-        p1.pop();
-
-        System.out.println(p1.toString() + " Size: " + p1.size());
     }
 }
--- src/models/FilaFIFO.java
+++ src/models/FilaFIFO.java
@@ -3,31 +3,62 @@ package models;
 public class FilaFIFO {
 
     private int fila[];
+    private int indice_ultimo;
 
     public FilaFIFO() {
         this.fila = new int[10];
+        this.indice_ultimo = -1;
     }
 
     public FilaFIFO(int tamanho) {
         this.fila = new int[tamanho];
+        this.indice_ultimo = -1;
     }
 
-    public void enqueue() {
+    public void enqueue(int num) {
+        fila[indice_ultimo+1] = num;
+        indice_ultimo++;
     }
 
-    public void dequeue() {
+    public int dequeue() {
+        int num = fila[0];
+        for(int i = 0; i < indice_ultimo; i++) {
+            fila[i] = fila[i+1];
+        }
+        indice_ultimo--;
+        return num;
     }
 
-    public void rear() {
+    public int rear() {
+        return fila[indice_ultimo];
     }
 
-    public void front() {
+    public int front() {
+        return fila[0];
     }
 
-    public void size() {
+    public int size() {
+        return indice_ultimo + 1;
     }
 
-    public void isEmpty() {
+    public boolean isEmpty() {
+        return indice_ultimo == -1 ? true : false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        if (!this.isEmpty()) {
+            for (int i = 0; i <= indice_ultimo; i++) {
+                sb.append(fila[i]);
+                if (i < indice_ultimo) {
+                    sb.append(", ");
+                }
+            }
+        }
+        sb.append("]");
+        return sb.toString();
     }
 
 }
