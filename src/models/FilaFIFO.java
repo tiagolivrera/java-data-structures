bdf768c0bc70a211c75e91492ba0cc3d989aadc3
Thyago de Oliveira Freitas <thyago.olifreitas@gmail.com>
2022-08-20 21:53:27 -0300

    Upload FilaFIFO

---
+++ src/models/FilaFIFO.java
@@ -0,0 +1,33 @@
+package models;
+
+public class FilaFIFO {
+
+    private int fila[];
+
+    public FilaFIFO() {
+        this.fila = new int[10];
+    }
+
+    public FilaFIFO(int tamanho) {
+        this.fila = new int[tamanho];
+    }
+
+    public void enqueue() {
+    }
+
+    public void dequeue() {
+    }
+
+    public void rear() {
+    }
+
+    public void front() {
+    }
+
+    public void size() {
+    }
+
+    public void isEmpty() {
+    }
+
+}
